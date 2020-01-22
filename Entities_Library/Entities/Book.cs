afb99b6ffac682c3b4ba2d1615b6b61e2327172a
Trang LUU <luudesmottes.trang@gmail.com>
2020-01-22 15:59:02 +0100

    Ajoutez des fichiers projet.

---
+++ Entities_Library/Entities/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_Library.Entities
+{
+    public class Book
+    {
+        #region Attributs
+        private int id;
+        private string authors;
+        private string title;
+        private string publisher;
+        private string myVar;
+
+        public string MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+
+        #endregion
+        #region Properties
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Authors
+        {
+            get { return authors; }
+            set { authors = value; }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+        #endregion
+        #region Constructor
+        #endregion
+        #region Functions
+        #endregion
+    }
+}
